7b4ed601479aae8911d7a22581a09de670d7f1d7
Alexei <alexey.belozerov111@gmail.com>
2021-08-10 04:14:10 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header className='header'>
+    <img src='https://cdn5.vectorstock.com/i/thumb-large/44/24/beauty-flowers-design-logo-vector-19124424.jpg' alt=''></img>
+  </header>
+)
+
+export default Header
